1b5cf9dfdea286582fecc42f2e06121e81b6777a
Diego Sarzi <diegosarzi@estudiorodapiao.com.br>
2013-08-28 02:55:08 -0300

    Loop 3 utilizando o Do

---
+++ loop3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	do
+	{
+	cout << "\n\nSeja bem vindo ao loop3" << endl;
+	cout << "Digite * para sair!" << endl;
+	}
+	while (cin.get() != '*');
+
+	return 0;
+}
